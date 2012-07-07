e11d8634475958d75b88e5060e2299013648d69c
Sean Kelly <github@katron.org>
2012-07-07 17:19:19 -0500

    Process each th element properly
    Each 'table.sports-reference th' element will have the right class and
    an event handler associated in order to sort the right column.

--- sr-table-sort.js
+++ sr-table-sort.js
@@ -1,5 +1,26 @@
 (function ($) {
     function initialize() {
+        var process_table = function() {
+            var $table = $(this);
+            var column = 1;
+
+            var process_header = function() {
+                var $th = $(this);
+                $th.data('nth', column);
+                $th.on('click', sort_column);
+                $th.toggleClass('sortable');
+                column++;
+            }
+
+            $table.find('th').each(process_header);
+        }
+
+        $('table.sports-reference').each(process_table);
+    }
+
+    function sort_column(ev) {
+        var $th = $(ev.target);
+        console.log($th);
     }
 
     $(document).ready(initialize);
