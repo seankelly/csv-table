e1e5fbcbf196528bc4e7e71ceeca1338c49b66c0
Sean Kelly <github@katron.org>
2012-07-06 21:14:17 -0500

    Parse shortcodes in comments
    It requires a bit of redirection to get it so I parse just for the [sr]
    shortcode. I could grab the regex from shortcode.php, but it doesn't do
    much for me since it uses the global $shortcode_tags variable.

--- sports-reference.php
+++ sports-reference.php
@@ -12,12 +12,56 @@ Version: 0.1
 Author URI:
 */
 
-function sr_shortcode_cb($attrs, $content=null) {
-    if (is_null($content)) {
-        return '';
+class srTable {
+    public function shortcode_in_comment($content) {
+        global $shortcode_tags;
+
+        if (empty($shortcode_tags) || !is_array($shortcode_tags)) {
+            return $content;
+        }
+
+        $_shortcode_tags = $shortcode_tags;
+        $shortcode_tags = array();
+
+        add_shortcode('sr', array('srTable', 'sr_shortcode_cb'));
+        $new_content = do_shortcode($content);
+
+        $shortcode_tags = $_shortcode_tags;
+
+        return $new_content;
     }
 
-    // Content is not null. Assume it's CSV data and try to make sense of it.
+    public function sr_shortcode_cb($attrs, $content=null) {
+        if (is_null($content)) {
+            return '';
+        }
+
+        // Content is not null. Assume it's CSV data and try to make sense of it.
+        $rows = str_getcsv($content, "\n");
+        foreach ($rows as &$row) {
+            $fields = str_getcsv($row);
+
+            // Count the number of a-z character to try guess if it's a header.
+            $count = preg_match_all('/[a-z]/i', $row, $matches);
+            $len = strlen($row);
+            if (($count / $len) > 0.5) {
+                $is_header = TRUE;
+                $opening_tag = '<td>';
+                $closing_tag = '</td>';
+            }
+            else {
+                $is_header = FALSE;
+                $opening_tag = '<th>';
+                $closing_tag = '</th>';
+            }
+
+            $html = $opening_tag
+                . implode($closing_tag . $opening_tag, $fields)
+                . $closing_tag;
+        }
+
+        return '<!-- sr -->';
+    }
 }
 
-add_shortcode('sr', 'sr_shortcode_cb');
+add_filter('comment_text', array('srTable', 'shortcode_in_comment'));
