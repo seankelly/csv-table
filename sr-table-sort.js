3f10efff3d74a95a43f8353712bc298663a9c51a
Sean Kelly <github@katron.org>
2012-07-07 21:36:41 -0500

    Support asc, desc, and unsorted modes
    Involved keeping the current mode saved and then keying off that. Now
    the hard part: actually sorting the table while still keeping the
    original order saved somewhere to revert the sorting.

--- sr-table-sort.js
+++ sr-table-sort.js
@@ -7,7 +7,7 @@
             var process_header = function() {
                 var $th = $(this);
                 $th.data('nth', column);
-                $th.data('sort', undefined);
+                $th.data('sort', {'order': undefined});
                 $th.on('click', sort_column);
                 $th.toggleClass('sortable');
                 column++;
@@ -22,12 +22,33 @@
     function sort_column(ev) {
         var $th = $(ev.target);
         var column = $th.data('nth');
-        $th.toggleClass('sorted');
+        var sort = $th.data('sort');
+        var next_sort = {'order': undefined};
+        var handle_class = function () {};
+
+        if (sort.order === undefined) {
+            next_sort.order = 'desc';
+            handle_class = function (th) {
+                th.addClass('sorted');
+            }
+        } else if (sort.order === 'desc') {
+            next_sort.order = 'asc';
+            // Still sorted, don't do anything with the 'sorted' class.
+        } else {
+            next_sort.order = undefined;
+            handle_class = function (th) {
+                th.removeClass('sorted');
+            }
+        }
+
+        handle_class($th);
         var $table = $th.parents('table.sports-reference');
         var selector = 'tr td:nth-child(' + column + ')';
         $table.find(selector).each(function() {
-            $(this).toggleClass('sorted');
+            handle_class($(this));
         });
+
+        $th.data('sort', next_sort);
     }
 
     $(document).ready(initialize);
