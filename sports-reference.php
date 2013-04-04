8a8cda3677d315bc15aeffd095748c68e5cd940e
Sean Kelly <code@katron.org>
2013-04-04 17:16:32 -0500

    Add GPLv2 (or later) license

--- sports-reference.php
+++ sports-reference.php
@@ -1,4 +1,22 @@
 <?php
+/*
+Copyright (C) 2012-2013 Sean Kelly
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 /**
  * @package sports-reference
  * @version 1.5
@@ -10,6 +28,7 @@ Description: Displays CSV data from a Sports Reference site in a vaguely Sports 
 Author: Sean Kelly
 Version: 1.5
 Author URI:
+License: GPL2
 */
 
 class srTable {
