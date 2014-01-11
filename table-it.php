751f9739b72fc52eb594bce6edeaaee22a9ecd68
Sean Kelly <code@katron.org>
2014-01-10 21:22:16 -0600

    Update copyright year

--- table-it.php
+++ table-it.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Copyright (C) 2012-2013 Sean Kelly
+Copyright (C) 2012-2014 Sean Kelly
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
